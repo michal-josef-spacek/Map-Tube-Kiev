3f7ffe70f953339e53d2930384f58cac23731df7
Michal Špaček <tupinek@gmail.com>
2015-01-10 14:21:39 +0100

    Add EXAMPLE3 section to doc.

--- Kiev.pm
+++ Kiev.pm
@@ -147,6 +147,44 @@ For more information about Kiev Map, click L<here|https://en.wikipedia.org/wiki/
  # Output like:
  # XML file: .*/kiev-map.xml
 
+=head1 EXAMPLE3
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Map::Tube::GraphViz;
+ use Map::Tube::GraphViz::Utils qw(node_color_without_label);
+ use Map::Tube::Kiev;
+
+ # Object.
+ my $obj = Map::Tube::Kiev->new;
+
+ # GraphViz object.
+ my $g = Map::Tube::GraphViz->new(
+         'callback_node' => \&node_color_without_label,
+         'driver' => 'neato',
+         'tube' => $obj,
+ );
+
+ # Get graph to file.
+ $g->graph('Kiev.png');
+
+ # Print file.
+ system "ls -l Kiev.png";
+
+ # Output like:
+ # -rw-r--r-- 1 skim skim 163162 Jan 10 14:20 Kiev.png
+
+=begin html
+
+<a href="https://raw.githubusercontent.com/tupinek/Map-Tube-Kiev/master/images/ex3.png">
+  <img src="https://raw.githubusercontent.com/tupinek/Map-Tube-Kiev/master/images/ex3.png" alt="Київський метрополітен" width="300px" height="300px" />
+</a>
+
+=end html
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
